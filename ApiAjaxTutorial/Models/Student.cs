0c923c272b3367fd6b84a34a9f3c41b7b74f3ece
Arpita <appypro90@gmail.com>
2019-05-09 13:47:32 +0530

    Add project files.

---
+++ ApiAjaxTutorial/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiAjaxTutorial.Models
+{
+    public class Student
+    {
+        public int studentID { get; set; }
+        public string studentName { get; set; }
+        public string studentSurname { get; set; }
+    }
+}
